25e0806685e8f814e95cf82730030e9038232b94
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-08-05 20:47:31 +0200

    First commit

---
+++ JS/controller.js
@@ -0,0 +1,30 @@
+
+
+var app = angular.module("yoTudni", [])
+
+app.controller("ctrl", function($scope){
+
+	
+	
+	
+	
+	
+	$scope.initSubject = function(subjectName){
+		
+		$scope.hideIcons(subjectName)
+		$scope.showContent(subjectName)
+		
+	}
+	
+	$scope.hideIcons = function(subjectName){
+		
+		//Fade out all the subjects
+		
+		
+	}
+	
+	
+})
+
+
+
