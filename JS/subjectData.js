762cd2b201c45e5345c0f9815e96777da81833d7
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-08-08 16:56:11 +0200

    Individual file for the configuration data.

---
+++ JS/subjectData.js
@@ -0,0 +1,36 @@
+
+
+var subjectData = {
+		
+	matek : {
+		imgName : "matek.png",
+		color : "#FC5E5E",
+		index : 1,
+		name : "matek",
+		teacher : "A",
+	},
+
+	biologia : {
+		imgName : "biologia.png",
+		color : "#83CB89",
+		index : 2,
+		name : "biologia",
+		teacher : "B",
+	},
+
+	kemia : {
+		imgName : "kemia.png",
+		color : "#C4699E",
+		index : 3,
+		name : "kemia",
+		teacher : "C",
+	},
+	
+	fizika : {
+		imgName : "fizika.png",
+		color : "#00A8C5",
+		index : 4,
+		name : "fizika",
+		teacher : "D",
+	}
+}
