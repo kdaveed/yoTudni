23f6e901a81aa6f28f38bd408014c452bdb82394
kdaveed <konkolydavid@konkolys-mbp.t.hu>
2017-08-11 13:48:27 +0200

    Component based subject loading works

--- JS/subjectData.js
+++ JS/subjectData.js
@@ -8,6 +8,7 @@ var subjectData = {
 		index : 1,
 		name : "matek",
 		teacher : "A",
+		shown :  true,
 	},
 
 	biologia : {
@@ -16,6 +17,7 @@ var subjectData = {
 		index : 2,
 		name : "biologia",
 		teacher : "B",
+		shown :  true,
 	},
 
 	kemia : {
@@ -24,6 +26,7 @@ var subjectData = {
 		index : 3,
 		name : "kemia",
 		teacher : "C",
+		shown :  true,
 	},
 	
 	fizika : {
@@ -32,5 +35,37 @@ var subjectData = {
 		index : 4,
 		name : "fizika",
 		teacher : "D",
+		shown :  true,
 	}
 }
+
+var subjectList = [
+		 {
+			imgName : "matek.png",
+			color : "#FC5E5E",
+			index : 1,
+			name : "matek",
+			teacher : "A",
+			shown :  true,
+		}, {
+			imgName : "biologia.png",
+			color : "#83CB89",
+			index : 2,
+			name : "biologia",
+			teacher : "B",
+			shown :  true,
+		}, {
+			imgName : "kemia.png",
+			color : "#C4699E",
+			index : 3,
+			name : "kemia",
+			teacher : "C",
+			shown :  true,
+		}, {
+			imgName : "fizika.png",
+			color : "#00A8C5",
+			index : 4,
+			name : "fizika",
+			teacher : "D",
+			shown :  true,
+		}]
