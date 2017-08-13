c7a6af8cf22ad827e854afff8a520961a061bdfa
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-08-13 12:19:11 +0200

    Proper div expansion works

--- JS/mosaikDescriptor.js
+++ JS/mosaikDescriptor.js
@@ -1,24 +1,38 @@
 
-
-
 var Mosaik = {
 
 	config : {
 		rows : 2,
-		cols : 3,
+		cols : 2,
 		dist : 30,
-		width : 150,
-		height : 70,
+		width : 100,
+		height : 100,
 	}
-			
-
 }
 
+var calculateCoords = function(array, config){
+	
+	if((config.rows*config.cols) != array.length ){
+		console.error("Mosaic configuration and input array are not compatible")
+	}
+	var cols = config.cols
+	var rows = config.rows
+	for(var i = 0; i < (rows*cols); i++){
+		var x = (i + cols) % cols
+		var y = Math.floor(i / cols )
+		array[i].coords = {
+			left : config.dist + x * (config.width + config.dist),
+			top :  config.dist + y * (config.height + config.dist)
+		}
+	}
+	return array
+}
+
+calculateCoords(subjectList, Mosaik.config)
+console.log(subjectList)
 //Writing the algorithms to generate the
 
-
 //The task is to calculate for each element 
-
 var preCalculation = function(config){
 	
 	var cols = config.cols
@@ -28,18 +42,20 @@ var preCalculation = function(config){
 		var x = (i + cols) % cols
 		var y = Math.floor(i / cols )
 		console.log("x : " + x + " y : " + y)
+		
 		object[i] = new Object()
+		object[i].top
+		/* This is currently not needed ...
 		object[i].top = y   
 		object[i].right = cols - x
 		object[i].bottom = rows - y
-		object[i].left = x
+		object[i].left = x  */
 	}
-	console.log(object)	
+	console.log(object)		
 }
 
 var getX = function(cols, i){
 	return (i + cols) % cols
 }
 
-
-preCalculation(Mosaik.config)
+//preCalculation(Mosaik.config)
